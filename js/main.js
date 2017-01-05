26e47063f33c8a697613d49e23823669faf4649d
jsirera <judsirera@gmail.com>
2017-01-05 15:40:51 +0100

    TV user profile

--- js/main.js
+++ js/main.js
@@ -17,12 +17,11 @@ var unregister = function() {
 
 var downloadPhotosList = function() {
 	console.log("downloadPhotosList() called");
-	
+
 	var url = base_url + endpoints.photos;
 	var params = { client_id: client_id };
 
 	$.get(url, params).done(function(data) {
-		console.log(data);
 		var template = $("#template").html();
 		var i = 1;
 		data.forEach(function(photo) {
@@ -33,21 +32,61 @@ var downloadPhotosList = function() {
 					username: photo.user.username,
 					location: photo.user.location,
 					date: photo.created_at.split("T")[0],
-					likes: photo.likes					
+					likes: photo.likes
 			};
-			 
-			
+
 			if (i == 1) {
 				params.active = "active";
 			}
-			console.log(photo.color);
 			i++;
-			
+
 			var rendered = Mustache.render(template, params);
 			$("#carousel").append(rendered);
-			
 		});
 	});
+}
+
+var downloadProfileUser = function(username) {
+	console.log("downloadProfileUser() called");
+
+	var url_profile = base_url + endpoints.users + "/" + username;
+	var url_photos = url_profile + "/" + endpoints.photos;
+	var params = { client_id: client_id };
+
+	$.get(url_profile, params).done(function(data) {
+		console.log(data);
+
+		var template = $("#userInformationTemplate").html();
+		params_ = {
+			image: data.profile_image.large,
+			username: data.username,
+			name: data.name,
+			posts: data.total_photos,
+			followers: data.followers_count,
+			following: data.following_count
+		}
+
+		var rendered = Mustache.render(template, params_);
+		$("#userInformation").append(rendered);
+	});
+
+	params.per_page = 30;
+	$.get(url_photos, params).done(function(data) {
+		var template = $("#userPostsTemplate").html();
+
+		data.forEach(function(photo) {
+			console.log(photo);
+
+			params_ = {
+				image: photo.urls.small
+			}
+
+			var rendered = Mustache.render(template, params_);
+			$("#userPosts").append(rendered);
+		});
+	});
+
+
 }
 
 //Initialize function
@@ -56,11 +95,12 @@ var init = function () {
     if ( backEventListener !== null ) {
         return;
     }
-    
+
     // TODO:: Do your initialization job
     console.log("init() called");
     downloadPhotosList();
-    
+		downloadProfileUser("vingtcent");
+
     var backEvent = function(e) {
         if ( e.keyName == "back" ) {
             try {
@@ -78,7 +118,7 @@ var init = function () {
             }
         }
     }
-    
+
     // add eventListener for tizenhwkey (Back Button)
     document.addEventListener( 'tizenhwkey', backEvent );
     backEventListener = backEvent;
